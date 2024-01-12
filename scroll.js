e93d8828239b0c7adb30c2cbd86d105e713fa792
Tyler Wright <tyler.wright5146@gmail.com>
2024-01-12 07:59:14 -0700

    arrow keys

--- scroll.js
+++ scroll.js
@@ -15,24 +15,44 @@ for (let i = 0; i < pages.length; i++) {
 const dots = document.querySelectorAll('.dot');
 
 let onscroll = () => {
-    const currentScrollPos = pageContainer.scrollTop;
-    pages.forEach((page, index) => {
-      const pageTop = page.offsetTop;
-      const pageBottom = pageTop + page.offsetHeight;
-      if (currentScrollPos >= pageTop && currentScrollPos < pageBottom) {
-        dots.forEach((dot, dotIndex) => {
-          if (dotIndex === index) {
-            dot.classList.add('active');
-            dot.style.transform = 'scale(1.5)'; // increase dot size
-          } else {
-            dot.classList.remove('active');
-            dot.style.transform = 'scale(1)'; // reset dot size
-          }
-        });
-      }
-    });
-  };
-  
-  // Update the active dot and resize dots on scroll
-  pageContainer.addEventListener('scroll', onscroll);
-  onscroll();
+  const currentScrollPos = pageContainer.scrollTop;
+  pages.forEach((page, index) => {
+    const pageTop = page.offsetTop;
+    const pageBottom = pageTop + page.offsetHeight;
+    if (currentScrollPos >= pageTop && currentScrollPos < pageBottom) {
+      dots.forEach((dot, dotIndex) => {
+        if (dotIndex === index) {
+          dot.classList.add('active');
+          dot.style.transform = 'scale(1.5)'; // increase dot size
+        } else {
+          dot.classList.remove('active');
+          dot.style.transform = 'scale(1)'; // reset dot size
+        }
+      });
+    }
+  });
+};
+
+pageContainer.addEventListener('scroll', onscroll);
+onscroll();
+
+function getCurrentPageIndex() {
+  let pageIndex = 0;
+  for (let i = 0; i < dots.length; i++) { 
+    if (dots[i].classList.contains('active')) {
+      pageIndex = i;
+    }
+  }
+  return pageIndex;
+}
+
+
+document.onkeydown = function (e) {
+  if (e.key == 38) {    
+    pages[Math.max(getCurrentPageIndex() - 1, 0)].scrollIntoView({ behavior: 'smooth' });
+  }
+
+  if (e.key == 40) {
+    pages[Math.min(getCurrentPageIndex() + 1, pages.length - 1)].scrollIntoView({ behavior: 'smooth' });
+  }
+};
